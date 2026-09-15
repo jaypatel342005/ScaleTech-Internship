bd07304edc9ac8f5365e5df4cfb4628e515e637b
jaypatel342005 <pateljay97378@gmail.com>
2026-09-15 19:24:18 +0530

    stage - 4 (15-09-2026)

--- Stage 4 - Django Basics/Basics/todo/urls.py
+++ Stage 4 - Django Basics/Basics/todo/urls.py
@@ -7,4 +7,7 @@ from . import views
 urlpatterns = [
     path("",views.todo, name="todo"),
     path("delete_todo/<int:id>",views.delete_todo, name="delete_todo"),
+    path("create_todo/",views.create_todo, name="create_todo"),
+    path("update_todo/<int:id>",views.update_todo, name="update_todo"),
+    path("status_todo/<int:id>",views.status_todo, name="status_todo"),
 ]
--- Stage 4 - Django Basics/Basics/todo/views.py
+++ Stage 4 - Django Basics/Basics/todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import * 
+from django.shortcuts import redirect
 
 # Create your views here.
 def todo(request):
@@ -9,6 +10,54 @@ def todo(request):
 
 def delete_todo(request,id):
     Todo.objects.get(id=id).delete()
-    return redirect("todo/")
+    return redirect("/todo/")
 
 
+def create_todo(request):
+    if request.method == "POST":
+        data = request.POST
+        title =  data.get('title')
+        desc = data.get('description')
+        prio = data.get('priority')
+        stat = data.get('status')
+
+        newTodo = Todo.objects.create(
+            title = title,
+            description = desc,
+            priority = prio,
+            status = stat,
+        )
+
+        return redirect("/todo/")
+    else:
+        return render(request, "todo/create_todo.html")
+
+def update_todo(request,id):
+    if request.method == "POST":
+        data = request.POST
+        title =  data.get('title')
+        desc = data.get('description')
+        prio = data.get('priority')
+        stat = data.get('status')
+
+        newTodo = Todo.objects.filter(id=id).create(
+            title = title,
+            description = desc,
+            priority = prio,
+            status = stat,
+        )
+
+        return redirect("/todo/")
+    else:
+        my_todo = Todo.objects.get(id=id)
+        return render(request, "todo/update_todo.html", {"todo": my_todo})
+
+def status_todo(request,id):
+    my_todo = Todo.objects.get(id=id)
+    if my_todo.status == 'PENDING':
+        my_todo.status = 'COMPLETED'
+    else:
+        my_todo.status = 'PENDING'
+    my_todo.save()
+    return redirect("/todo/")
+        
